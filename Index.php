045fbd5bbdfbb7a3f2acb404adac43151ba05385
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2024-12-22 21:03:51 +0300

    php Fuck You

--- Index.php
+++ Index.php
@@ -8,7 +8,7 @@
 <body>
     <?php
        $i = 0;
-
+/*
        // Çift sayıların toplamı için bir değişken       
        // While döngüsü ile 0-100 arasındaki sayıları kontrol et
        while ($i <= 100) {
@@ -107,7 +107,7 @@
 /* Harika! Şimdi seviyeyi biraz daha artırarak bir problem çözümü yapalım. 1 ile 50 arasındaki sayıları döngüde kontrol edeceğiz ve:
             3'e bölünebilen sayılar için "3'e bölünebilen sayı: X",
             5'e bölünebilen sayılar için "5'e bölünebilen sayı: X",
-            Hem 3'e hem 5'e bölünebilen sayılar için "3 ve 5'e bölünebilen sayı: X" yazdıracağız. */
+            Hem 3'e hem 5'e bölünebilen sayılar için "3 ve 5'e bölünebilen sayı: X" yazdıracağız. 
            
 
              for ($i=1; $i <= 50 ; $i++) { 
@@ -200,6 +200,109 @@
                 }
             }
           
+
+          for($i=1;$i<=6; $i++)
+		{
+			for($k=6;$k>=$i;$k--)
+			{
+				echo "*";
+			}
+			echo "<br>";
+		}
+
+
+
+	$IndirimKodu="Kartal";
+	$fiyat=1000;
+
+	switch ($IndirimKodu) {
+		case 'Kartal':
+			echo"indirim ugulanmış fiyat".$fiyat * 0.20;	
+			break;
+		case 'civciv':
+			echo"indirim ugulanmış fiyat".$fiyat * 0.50;	
+			break;
+		case 'BabaErzincan':
+			echo"indirim ugulanmış fiyat".$fiyat * 0.76;	
+			break;
+		
+		default:
+			echo"Ne anlation";
+			break;
+	}
+
+	if ($IndirimKodu == "Kartal") {
+		echo"indirim ugulanmış fiyat".$fiyat * 0.20."<br>";
+	} else {
+		# code...
+	}
+		//kişinin kilosu 60 dan azsa küçük 60 80 normal 80 üstü kilolu
+
+		$kilo=55;
+
+			switch ($kilo) {
+				case $kilo <= 60: echo "kilonuz zayıf"; break;
+				case $kilo >= 60 && $kilo <= 80: echo"kilonuz normal";break;
+				case $kilo >= 80: echo"kilonuz fazla";break;
+				
+				default:
+					echo"Böyle bir aralık bulınmamakta";
+					break;
+			}
+
+		//50 ile 100 arası çift asyıları ekrana ayz<dıran program 
+	
+
+			$i=1;
+
+			while ($i <= 50) {
+				if ($i % 2 == 0) {
+					echo"<br>".$i;
+				}
+				$i++;
+			}
+
+
+echo"<hr/>";
+
+    //Çarpım Tablosu
+
+
+
+        for ($i=1; $i <=10 ; $i++) { 
+            for ($j=1; $j <=10 ; $j++) { 
+                $Exam=$i*$j;
+                echo"$i x $j=$Exam <br>";
+            }
+            echo"<br>";
+        }
+
+*/
+
+//1'den 20'ye kadar olan sayıların toplamını bulan bir program yazmak için hangi döngü kullanılır ve bu döngü nasıl yazılır?
+
+
+$toplam = 0;
+for ($i=1; $i <=20 ; $i++) { 
+    $toplam += $i;
+}
+echo"Toplam $toplam";
+
+/*Soru 1:
+Bir sayı verildiğinde, bu sayının pozitif, negatif veya sıfır olduğunu belirlemek için bir program nasıl yazılır?
+
+Soru 2:
+1'den 20'ye kadar olan sayıların toplamını bulan bir program yazmak için hangi döngü kullanılır ve bu döngü nasıl yazılır?
+
+Soru 3:
+Bir kullanıcıdan alınan bir sayının, 1'den 10'a kadar olan sayılarla çarpımını yazdıran bir program nasıl yazılır?
+
+Soru 4:
+Bir sayı verildiğinde, bu sayının tek mi çift mi olduğunu kontrol eden bir program yazınız. Hangi koşul kullanılmalıdır?
+
+Soru 5:
+Bir dizi içerisindeki tüm sayıların toplamını bulan bir PHP programında hangi döngü ve koşullar kullanılabilir? */
+
          ?>
 </body>
 </html>
